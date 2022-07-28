48baf4ea3dbf9fb065378d8917c8fc82fc0ef0b9
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-07-28 20:20:07 +0200

    Update index file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './css/index.css';
 import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/F-E-M-Clipboard-Design">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
